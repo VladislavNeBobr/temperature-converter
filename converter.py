33d19224e4735b0cf02cc7a1a319df0ad68c768c
Владислав Криськів <vladislavkriskiv117@gmail.com>
2025-11-29 23:50:07 +0200

    Added comment

--- converter.py
+++ converter.py
@@ -4,6 +4,7 @@ def fahrenheit_to_celsius(fahrenheit):
 	return (fahrenheit - 32) * 5/9
 def celsius_to_kelvin(celsius):
 	return celsius + 273.15
+# v. 1.0.
 def kelvin_to_celsius(kelvin):
 	return kelvin - 273.15
 def fahrenheit_to_kelvin(f):
